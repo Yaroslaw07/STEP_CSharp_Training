6f4d7b429410727ddb54e558b951132c15727c2f
Yaroslaw07 <69744663+Yaroslaw07@users.noreply.github.com>
2021-03-19 13:09:05 +0200

    класс Bank вынесен в отдельный файл

--- BankSavePropertiesToFile/Program.cs
+++ BankSavePropertiesToFile/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Threading;
-using System.IO;
+
 
 namespace BankSavePropertiesToFile
 {
@@ -18,74 +18,5 @@ namespace BankSavePropertiesToFile
             A.percent = 12;
             Thread.Sleep(100);
         }
-    }
-
-    class Bank
-    {
-        private int Money;
-        private string Name;
-        private int Percent;
-        public int money
-        {
-            get
-            {
-                return Money;
-            }
-            set
-            {
-                Money = value;
-                ThreadPool.QueueUserWorkItem(SaveToFile);
-            }
-        }
-        public string name
-        {
-            get
-            {
-                return Name;
-            }
-            set
-            {
-                Name = value;
-                ThreadPool.QueueUserWorkItem(SaveToFile);
-            }
-        }
-
-        public int percent
-        {
-            get
-            {
-                return Percent;
-            }
-            set
-            {
-                Percent = value;
-                ThreadPool.QueueUserWorkItem(SaveToFile);
-            }
-        }
-
-        public Bank()
-        {
-
-        }
-
-        private void SaveToFile(object StateInfo)
-        {
-            File.WriteAllText("file1.txt",GetStringOfProperties());
-        }
-
-
-        private string GetStringOfProperties()
-        {
-            var props = GetType().GetProperties();
-            string result = GetType().Name;
-            result += ": ";
-
-            foreach (var property in props)
-            {
-                result += $"{property.Name} = {property.GetValue(this)} | ";
-            }
-            return result;
-        }
-
     }
 }
