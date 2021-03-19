d2922e55bbe982584d1f7701178f5d509d4add40
Yaroslaw07 <69744663+Yaroslaw07@users.noreply.github.com>
2021-03-19 12:45:07 +0200

    Добавьте файлы проекта.

---
+++ BankSavePropertiesToFile/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace BankSavePropertiesToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank A = new Bank();
+            A.money = 40;
+            Thread.Sleep(100);
+
+            A.name = "ffad";
+            Thread.Sleep(100);
+
+            A.percent = 12;
+            Thread.Sleep(100);
+        }
+    }
+
+    class Bank
+    {
+        private int Money;
+        private string Name;
+        private int Percent;
+        public int money
+        {
+            get
+            {
+                return Money;
+            }
+            set
+            {
+                Money = value;
+                ThreadPool.QueueUserWorkItem(SaveToFile);
+            }
+        }
+        public string name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+                ThreadPool.QueueUserWorkItem(SaveToFile);
+            }
+        }
+
+        public int percent
+        {
+            get
+            {
+                return Percent;
+            }
+            set
+            {
+                Percent = value;
+                ThreadPool.QueueUserWorkItem(SaveToFile);
+            }
+        }
+
+        public Bank()
+        {
+
+        }
+
+        private void SaveToFile(object StateInfo)
+        {
+            File.WriteAllText("file1.txt",GetStringOfProperties());
+        }
+
+
+        private string GetStringOfProperties()
+        {
+            var props = GetType().GetProperties();
+            string result = GetType().Name;
+            result += ": ";
+
+            foreach (var property in props)
+            {
+                result += $"{property.Name} = {property.GetValue(this)} | ";
+            }
+            return result;
+        }
+
+    }
+}
---
+++ ShowNumberOfThread/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace ShowNumberByThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a digit from 1 to 7: ");
+            int numbOfShow = int.Parse(Console.ReadLine());
+
+            while(numbOfShow <= 0 || numbOfShow >7)
+            {
+                Console.WriteLine("Wrong number. Try again:");
+                numbOfShow = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 1; i <= numbOfShow; i++)
+            {
+                Thread t = new Thread(PrintNumber);
+                t.Start(i);
+            }
+
+            Thread.Sleep(1000);
+        }
+
+        public static void PrintNumber(object numb)
+        {
+            int numbToShow = (int)numb;
+            for (int i = 0; i < 100; i++)
+            {
+                string tab = string.Empty;
+                for (int j = 1; j < numbToShow; j++)
+                {
+                    tab += "\t";
+                }
+                string res = tab + numbToShow;
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
